b840838c613d686c127692b803edbbd3135c7476
Jason J. Gullickson <jason.gullickson@gmail.com>
2016-07-24 21:52:56 -0500

    Initial fleshing out of server components

---
+++ server.js
@@ -0,0 +1,11 @@
+const spawn = require('child_process').spawn;
+
+// TODO: Start HTTP server
+
+// TODO: Respond to HTTP server events
+
+// TODO: Compare Accept header to supported media types
+
+// TODO: Spawn subprocess to transcode source media into destination format
+
+// TODO: Handle transcoder events
